7434958ea5422f0ca30f3d4ecf9dea26a88eee12
Vikky yadav <146602351+Vikashya12@users.noreply.github.com>
2024-05-18 17:34:19 +0530

    Update ai.py

--- SHUKLA/plugins/tools/ai.py
+++ SHUKLA/plugins/tools/ai.py
@@ -30,7 +30,7 @@ async def chat_gpt(bot, message):
                     end_time = time.time()
                     telegram_ping = str(round((end_time - start_time) * 1000, 3)) + " ms"
                     await message.reply_text(
-                        f" {x}      ᴀɴsᴡᴇʀɪɴɢ ʙʏ ➛  @OWNER_DHPR",
+                        f" {x}      ᴀɴsᴡᴇʀɪɴɢ ʙʏ ➛ [ DHPR ](https://t.me/OWNER_DHPR) 🌹**",
                         parse_mode=ParseMode.MARKDOWN
                     )
                 else:
