90422da0cf8b552c16ba7d08c058b650365cf138
Vikky yadav <146602351+Vikashya12@users.noreply.github.com>
2024-05-19 21:56:32 +0530

    Update restart.py

--- SHUKLA/plugins/owner/restart.py
+++ SHUKLA/plugins/owner/restart.py
@@ -10,8 +10,8 @@ from ... import *
 async def restart(client: Client, message: Message):
     reply = await message.reply_text("**Restarting...**")
     await message.delete()
-    await reply.edit_text("Successfully Restarted ShuklaBot...\n\n💞 Wait 1-2 minutes\nLoad plugins...</b>")
-    os.system(f"kill -9 {os.getpid()} && python3 -m SHUKLA")
+    await reply.edit_text("Successfully Restarted DHPRBot...\n\n💞 Wait 1-2 minutes\nLoad plugins...</b>")
+    os.system(f"kill -9 {os.getpid()} && python3 -m DHPR")
   
 
 __NAME__ = "Rᴇsᴛᴀʀᴛ"
